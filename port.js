fa1bfc0233a0a423b986238c3b1285c9ab628789
Alabi12 <wolo22001@gmail.com>
2023-01-05 21:12:25 +0000

    update the nav bar css

--- port.js
+++ port.js
@@ -62,3 +62,16 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
+window.onscroll = function() {myFunction()};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
