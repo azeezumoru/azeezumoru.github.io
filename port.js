900928833dbb875749ac0e0de385e34bc2eeef99
Bright <101227283+brightbrain20@users.noreply.github.com>
2023-01-06 11:03:38 +0100

    Update port.js
    Updated port.js file

--- port.js
+++ port.js
@@ -3,6 +3,7 @@ function openNav() {
   document.getElementById('myNav').style.width = '100%';
 }
 
+
 // eslint-disable-next-line no-unused-vars
 function closeNav() {
   document.getElementById('myNav').style.width = '0%';
